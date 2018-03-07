ddf278a68e1b398d0426f86042d1279ab102aee9
Gabriel FOURNIER <gabfou95@gmail.com>
2018-03-07 14:41:30 +0100

    fdsf

--- level02/source.c
+++ level02/source.c
@@ -1,12 +1,14 @@
 
+#include <stdio.h>
 //  pas finit
 char* p()
 {
-	char truc2[104]; // taille a pas a lair de corespondre
-	fflush(NULL); // heuuuu
-	gets(truc2);
+	char truc2[76]; // taille a pas a lair de corespondre la ca devrait etre bon
 	int random2emeargumentnexteip // je sais pas comment le recuperer
-	if ((random2emeargumentnexteip & 0xb0000000) == 0xb0000000) // si ca commence par un b ca print l adresse de ebp et ca exit avant qu on puisse effacer etrenquille grace au exit
+
+	fflush(STDOUT_FILENO); // heuuuu
+	gets(truc2);
+	if ((random2emeargumentnexteip & 0xb0000000) == 0xb0000000) // si ca commence par un b ca print l adresse de ret et ca exit avant qu on puisse effacer etrenquille grace au exit
 	{
 		printf("(%p)\n", );
 		exit(1);  // grr
